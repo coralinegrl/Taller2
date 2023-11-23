1e4affa64450b2d0e2cc5f8922fef2daaaf2f0b2
Andrea Saldaña Cavour <82416715+coralinegrl@users.noreply.github.com>
2023-11-23 02:14:06 -0300

    Delete frontend/taller2 directory

--- frontend/taller2/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vue-router', // Ejemplo: agrega las dependencias que deseas transpilar aquí
-    'vuex',
-    // Otras dependencias que necesitan ser transpiladas
-  ]
-})
